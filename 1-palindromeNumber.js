1eda3ab23f61fc9251bb514484788b44bfcbd167
Jagadishvg <jagadishvg123@gmail.com>
2025-05-09 15:47:46 +0530

    Changes prettier

--- 1-palindromeNumber.js
+++ 1-palindromeNumber.js
@@ -1,7 +1,7 @@
 const isPalindrome = (x) => {
-    if (x < 0) return false;
+  if (x < 0) return false;
 
-    const res = x.toString().split("").reverse().join("")
-    return +res === x
-}
-console.log(isPalindrome(121))
+  const res = x.toString().split("").reverse().join("");
+  return +res === x;
+};
+console.log(isPalindrome(121));
